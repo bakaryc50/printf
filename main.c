2f1646f2a61d5b2569dee69ac5994ed1b1bb1673
mathias-tad <mathiasstadesse@gmail.com>
2021-07-27 09:27:04 -0700

    Mathias commit: printf/get_print_any_func, holberton, main, _printf

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <limits.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @Description: we are going to have two functions 1 to print
+ * the other to change string to character
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+        int len;
+        int len2;
+        _printf("String:[%s]\n", "I am a string !");
+        _printf("char: [%c]\n", 'H');
+        len = _printf("Percent:[%%]\n");
+        len2 = printf("Percent:[%%]\n");
+        _printf("Len:[%d]\n", len);
+        printf("Len:[%d]\n", len2);
+        return 0;
+}
